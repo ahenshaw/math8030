76dc2e0a9075d52ef711c0f223696ce65d07324d
Andrew Henshaw <andrew@henshaw.us>
2018-10-09 07:14:02 -0400

    Expanded Hungarian

--- hungarian.py
+++ hungarian.py
@@ -11,6 +11,21 @@ costs = '''
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 
+def pp(table, side=None):
+    if len(table.shape) == 1:
+        for v in table:
+            print('{:3}'.format(v), end='')
+        print()
+    else:
+        for i, row in enumerate(table):
+            for v in row:
+                print('{:3}'.format(v), end='')
+            if side is not None:
+                print(' | {:3}'.format(side[i][0]))
+            else:
+                print()
+
+
 costs = [[int(y) for y in x.split()] for x in (filter(None, costs.split('\n')))]
 costs = np.array(costs)
 
@@ -18,7 +33,29 @@ r, c = linear_sum_assignment(costs)
 out = []
 for row, col in zip(r,c):
     out.append('x{}{} = '.format(row+1, col+1))
+
+print('--------- Solution ----------')
 print('{}1'.format(''.join(out)))
 print('cost: {}'.format(costs[r,c].sum()))
+print('\n --------- Steps ----------')
+redo = costs.copy()
+print('Row minimums')
+min_rows = np.min(redo, axis=1).reshape((-1,1))
+pp(redo, min_rows)
+
+redo -= min_rows
+print('\nCol minimums')
+min_cols = np.min(redo, axis=0)
+pp(redo)
+print('-'*3*len(redo[0]))
+pp(min_cols)
+redo -= min_cols
+print()
+pp(redo)
+print('\nCover all zeros')
+print('Find min of uncovered cells = x')
+print('Subtract x from uncovered cells')
+print('Add x to intersection points of lines')
+print('Repeat until zero on each row and column')
 
 
